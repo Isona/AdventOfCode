48c11c3c51527c174160db2b3ba1d3ab7e1d6fb1
Izzy Whistlecroft <Izzy.Whistlecroft@microsoft.com>
2022-12-01 11:19:06 +0000

    Add 2022 day1

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,37 +1,64 @@
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    let input = parse_input(INPUT);
-
-    let part_1_answer = part_1(&input);
+    let part_1_answer = part_1(INPUT);
     println!("{part_1_answer}");
 
-    let part_2_answer = part_2(&input);
+    let part_2_answer = part_2(INPUT);
     println!("{part_2_answer}");
 }
 
-fn part_1(input: &[i32]) -> i32 {}
+fn part_1(input: &str) -> i32 {
+    let mut max_elf_sum = 0;
+    let mut elf_sum = 0;
 
-fn part_2(input: &[i32]) -> i32 {}
+    for line in input.lines() {
+        if line.trim().is_empty() {
+            max_elf_sum = elf_sum.max(max_elf_sum);
+            elf_sum = 0;
+        } else {
+            elf_sum += line.parse::<i32>().unwrap();
+        }
+    }
 
-fn parse_input(input: &str) -> Vec<i32> {
-    input.lines().map(|x| x.parse::<i32>().unwrap()).collect()
+    // Don't miss the last elf
+    max_elf_sum = elf_sum.max(max_elf_sum);
+
+    max_elf_sum
+}
+
+fn part_2(input: &str) -> i32 {
+    let mut elf_sum = 0;
+    let mut elf_sums: Vec<i32> = Vec::new();
+
+    for line in input.lines() {
+        if line.trim().is_empty() {
+            elf_sums.push(elf_sum);
+            elf_sum = 0;
+        } else {
+            elf_sum += line.parse::<i32>().unwrap();
+        }
+    }
+
+    // Don't miss the last elf
+    elf_sums.push(elf_sum);
+
+    elf_sums.sort_by(|a, b| b.cmp(a));
+    elf_sums[0..3].iter().sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const TESTINPUT: &str = include_str!("testinput.txt");;
+    const TESTINPUT: &str = include_str!("testinput.txt");
 
     #[test]
     fn part_1_test() {
-        let input = parse_input(TESTINPUT);
-        assert_eq!(part_1(&input), 7);
+        assert_eq!(part_1(TESTINPUT), 24000);
     }
 
     #[test]
     fn part_2_test() {
-        let input = parse_input(TESTINPUT);
-        assert_eq!(part_2(&input), 5);
+        assert_eq!(part_2(TESTINPUT), 45000);
     }
 }
