6507ffdb6d08fb9408254cc20ea0257d6af3d555
Izzy Whistlecroft <isabelwh@microsoft.com>
2022-12-02 16:51:07 +0000

    Add 2020 templates, and 2020 day1

---
+++ 2020-rs/day14/src/main.rs
@@ -0,0 +1,37 @@
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    let input = parse_input(INPUT);
+
+    let part_1_answer = part_1(&input);
+    println!("{part_1_answer}");
+
+    let part_2_answer = part_2(&input);
+    println!("{part_2_answer}");
+}
+
+fn part_1(input: &[i32]) -> i32 {}
+
+fn part_2(input: &[i32]) -> i32 {}
+
+fn parse_input(input: &str) -> Vec<i32> {
+    input.lines().map(|x| x.parse::<i32>().unwrap()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TESTINPUT: &str = include_str!("testinput.txt");
+
+    #[test]
+    fn part_1_test() {
+        let input = parse_input(TESTINPUT);
+        assert_eq!(part_1(&input), 7);
+    }
+
+    #[test]
+    fn part_2_test() {
+        let input = parse_input(TESTINPUT);
+        assert_eq!(part_2(&input), 5);
+    }
+}
