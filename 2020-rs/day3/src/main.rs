d9a2d99430c80bb27271f43d8e487a0c786f4662
Izzy Whistlecroft <isabelwh@microsoft.com>
2022-12-02 17:39:13 +0000

    Add 2020 day 3

--- 2020-rs/day3/src/main.rs
+++ 2020-rs/day3/src/main.rs
@@ -1,18 +1,64 @@
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    let input = parse_input(INPUT);
-
-    let part_1_answer = part_1(&input);
+    let part_1_answer = part_1(INPUT);
     println!("{part_1_answer}");
 
-    let part_2_answer = part_2(&input);
+    let part_2_answer = part_2(INPUT);
     println!("{part_2_answer}");
 }
 
-fn part_1(input: &[i32]) -> i32 {}
+fn part_1(input: &str) -> i32 {
+    let mut current_position = 0;
+    let mut trees_hit = 0;
 
-fn part_2(input: &[i32]) -> i32 {}
+    for line in input.lines() {
+        let line_chars: Vec<char> = line.chars().collect();
+        if line_chars[current_position] == '#' {
+            trees_hit += 1;
+        }
+        current_position = (current_position + 3) % line.len();
+    }
+
+    trees_hit
+}
+
+fn part_2(input: &str) -> i64 {
+    let mut current_y = 0;
+    let mut trees_hit_r1d1 = 0;
+    let mut trees_hit_r3d1 = 0;
+    let mut trees_hit_r5d1 = 0;
+    let mut trees_hit_r7d1 = 0;
+    let mut trees_hit_r1d2 = 0;
+
+    for line in input.lines() {
+        let line_chars: Vec<char> = line.chars().collect();
+        let line_len = line_chars.len();
+
+        if line_chars[(current_y) % line_len] == '#' {
+            trees_hit_r1d1 += 1;
+        }
+
+        if line_chars[(current_y * 3) % line_len] == '#' {
+            trees_hit_r3d1 += 1;
+        }
+
+        if line_chars[(current_y * 5) % line_len] == '#' {
+            trees_hit_r5d1 += 1;
+        }
+
+        if line_chars[(current_y * 7) % line_len] == '#' {
+            trees_hit_r7d1 += 1;
+        }
+
+        if current_y % 2 == 0 && line_chars[(current_y / 2) % line_len] == '#' {
+            trees_hit_r1d2 += 1;
+        }
+        current_y += 1;
+    }
+
+    trees_hit_r1d1 * trees_hit_r3d1 * trees_hit_r5d1 * trees_hit_r7d1 * trees_hit_r1d2
+}
 
 fn parse_input(input: &str) -> Vec<i32> {
     input.lines().map(|x| x.parse::<i32>().unwrap()).collect()
@@ -25,13 +71,11 @@ mod tests {
 
     #[test]
     fn part_1_test() {
-        let input = parse_input(TESTINPUT);
-        assert_eq!(part_1(&input), 7);
+        assert_eq!(part_1(&TESTINPUT), 7);
     }
 
     #[test]
     fn part_2_test() {
-        let input = parse_input(TESTINPUT);
-        assert_eq!(part_2(&input), 5);
+        assert_eq!(part_2(&TESTINPUT), 336);
     }
 }
