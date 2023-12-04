da1ece57ce582268981db8ed5eb55af08337d222
Izzy Whistlecroft <isona7@hotmail.co.uk>
2023-12-04 11:17:51 +0000

    Add partial 2022 day5

--- 2022/day5/src/main.rs
+++ 2022/day5/src/main.rs
@@ -1,21 +1,52 @@
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    let input = parse_input(INPUT);
+    let (crates, instructions) = parse_input(INPUT);
 
-    let part_1_answer = part_1(&input);
+    let part_1_answer = part_1(crates.clone(), &instructions);
     println!("{part_1_answer}");
 
-    let part_2_answer = part_2(&input);
+    let part_2_answer = part_2(crates, &instructions);
     println!("{part_2_answer}");
 }
 
-fn part_1(input: &[i32]) -> i32 {}
+fn part_1(crates: Vec<Vec<char>>, instructions: &Vec<(i32, i32, i32)>) -> String {
+    todo!()
+}
 
-fn part_2(input: &[i32]) -> i32 {}
+fn part_2(crates: Vec<Vec<char>>, instructions: &Vec<(i32, i32, i32)>) -> String {
+    todo!()
+}
 
-fn parse_input(input: &str) -> Vec<i32> {
-    input.lines().map(|x| x.parse::<i32>().unwrap()).collect()
+fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<(i32, i32, i32)>) {
+    let mut stack_count = 0;
+    let mut instructions = vec![];
+
+    // Input lines like: move 1 from 2 to 1
+    for line in input.lines() {
+        if line.starts_with("move") {
+            let split_line = line.split(" ").collect::<Vec<&str>>();
+            let count = split_line[1].parse::<i32>().unwrap();
+            let source = split_line[3].parse::<i32>().unwrap() - 1;
+            let dest = split_line[5].parse::<i32>().unwrap() - 1;
+            instructions.push((count, source, dest));
+        }
+        // Get the number of stacks
+        else if line.starts_with(" 1") {
+            stack_count = line
+                .split_whitespace()
+                .last()
+                .unwrap()
+                .parse::<usize>()
+                .unwrap();
+        } else if !line.is_empty() {
+        }
+    }
+
+    let mut crates = vec![vec![]; stack_count];
+
+    (crates, instructions)
+    // input.lines().map(|x| x.parse::<i32>().unwrap()).collect()(crates, instructions)
 }
 
 #[cfg(test)]
@@ -25,13 +56,13 @@ mod tests {
 
     #[test]
     fn part_1_test() {
-        let input = parse_input(TESTINPUT);
-        assert_eq!(part_1(&input), 7);
+        let (crates, instructions) = parse_input(TESTINPUT);
+        assert_eq!(part_1(crates, &instructions), "CMZ");
     }
 
     #[test]
     fn part_2_test() {
-        let input = parse_input(TESTINPUT);
-        assert_eq!(part_2(&input), 5);
+        let (crates, instructions) = parse_input(TESTINPUT);
+        assert_eq!(part_2(crates, &instructions), 5);
     }
 }
