30e067ecdde127a16edace882fc23e9b3191fe0d
Izzy Whistlecroft <isona7@hotmail.co.uk>
2023-12-08 12:28:19 +0000

    Implement custom structs for 2023 day5

--- 2023/day05/src/main.rs
+++ 2023/day05/src/main.rs
@@ -1,5 +1,3 @@
-use std::{collections::HashMap, ops::Range};
-
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
@@ -12,19 +10,13 @@ fn main() {
     println!("Part 2: {part_2_answer}");
 }
 
-fn part_1(seeds: &[u64], almanac_map: &[HashMap<Range<u64>, (u64, u64)>]) -> u64 {
-    let mut closest_location = u64::MAX;
+fn part_1(seeds: &[i64], almanac_map: &Vec<AlmanacMap>) -> i64 {
+    let mut closest_location = i64::MAX;
 
     for seed in seeds {
         let mut current_value = *seed;
         for stage in almanac_map {
-            for (map_range, (dest_range_start, src_range_start)) in stage {
-                if map_range.contains(&current_value) {
-                    let diff = current_value - src_range_start;
-                    current_value = dest_range_start + diff;
-                    break;
-                }
-            }
+            current_value = stage.map_value(current_value);
         }
 
         closest_location = current_value.min(closest_location);
@@ -33,9 +25,9 @@ fn part_1(seeds: &[u64], almanac_map: &[HashMap<Range<u64>, (u64, u64)>]) -> u64
     closest_location
 }
 
-fn part_2(seeds: &[u64], almanac_map: &[HashMap<Range<u64>, (u64, u64)>]) -> u64 {
+fn part_2(seeds: &[i64], almanac_map: &Vec<AlmanacMap>) -> i64 {
     // recalculate seeds vector
-    let mut new_seeds: Vec<u64> = Vec::new();
+    let mut new_seeds: Vec<i64> = Vec::new();
 
     for seed_range in seeds.chunks(2) {
         let range = seed_range[0]..seed_range[0] + seed_range[1];
@@ -46,42 +38,85 @@ fn part_2(seeds: &[u64], almanac_map: &[HashMap<Range<u64>, (u64, u64)>]) -> u64
     part_1(&new_seeds, almanac_map)
 }
 
-fn parse_input(input: &str) -> (Vec<u64>, Vec<HashMap<Range<u64>, (u64, u64)>>) {
+pub struct AlmanacMap(Vec<SeedTransformer>);
+
+impl AlmanacMap {
+    fn new() -> AlmanacMap {
+        AlmanacMap(Vec::new())
+    }
+
+    fn push(&mut self, input: SeedTransformer) {
+        self.0.push(input);
+    }
+
+    fn map_value(&self, input_value: i64) -> i64 {
+        for transformer in &self.0 {
+            if transformer.src_contains(input_value) {
+                return transformer.transform_value(input_value);
+            }
+        }
+
+        input_value
+    }
+}
+
+pub struct SeedTransformer {
+    src_start: i64,
+    len: i64, // Remember start+len is the first value outside the range
+    transformation: i64,
+}
+
+impl SeedTransformer {
+    fn src_contains(&self, value: i64) -> bool {
+        value >= self.src_start && value < self.src_start + self.len
+    }
+
+    fn transform_value(&self, value: i64) -> i64 {
+        value + self.transformation
+    }
+}
+
+fn parse_input(input: &str) -> (Vec<i64>, Vec<AlmanacMap>) {
     let mut lines = input.lines();
     // Seeds: 1 2 3 4
 
     // Parse seeds from the first line
 
-    let seeds: Vec<u64> = lines
+    let seeds: Vec<i64> = lines
         .next()
         .unwrap()
         .split(' ')
-        .filter_map(|x| x.parse::<u64>().ok())
+        .filter_map(|x| x.parse::<i64>().ok())
         .collect();
 
-    let mut stages: Vec<HashMap<Range<u64>, (u64, u64)>> = Vec::new();
+    let mut stages: Vec<_> = Vec::new();
 
     // For the rest of the lines
 
     // [dest range start] [source range start] [range length]
-    let mut stage: HashMap<Range<u64>, (u64, u64)> = HashMap::new();
+    let mut stage: AlmanacMap = AlmanacMap::new();
 
     for line in lines.skip(2) {
         if line.is_empty() {
             continue;
         }
 
-        let split_line: Vec<&str> = line.split(" ").collect();
+        let split_line: Vec<&str> = line.split(' ').collect();
         if split_line.len() == 2 {
             stages.push(stage);
-            stage = HashMap::new();
+            stage = AlmanacMap::new();
         } else {
-            let dest_range_start = split_line[0].parse::<u64>().unwrap();
-            let src_range_start = split_line[1].parse::<u64>().unwrap();
-            let range_length = split_line[2].parse::<u64>().unwrap();
+            let dest_start = split_line[0].parse::<i64>().unwrap();
+            let src_start = split_line[1].parse::<i64>().unwrap();
+            let len = split_line[2].parse::<i64>().unwrap();
+            let transformation = dest_start - src_start;
 
-            let range = src_range_start..src_range_start + range_length;
-            stage.insert(range, (dest_range_start, src_range_start));
+            let transformer = SeedTransformer {
+                src_start,
+                len,
+                transformation,
+            };
+            stage.push(transformer);
         }
     }
 
@@ -107,6 +142,3 @@ mod tests {
         assert_eq!(part_2(&seeds, &almanac_map), 46);
     }
 }
-
-// 1389615163 - too high
-// 525792406
