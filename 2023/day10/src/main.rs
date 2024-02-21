0405fd0692cad4a7ee3d9cd55e2817a7c7daa200
Izzy Whistlecroft <isona7@hotmail.co.uk>
2024-02-21 17:40:06 +0000

    Add aoc_lib, more work on 2023 day10

---
+++ 2023/day10/src/main.rs
@@ -0,0 +1,167 @@
+#![feature(let_chains)]
+
+use std::collections::{HashMap, HashSet};
+
+use aoc_lib::Direction;
+use aoc_lib::*;
+
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    let input = parse_input(INPUT);
+
+    let part_1_answer = part_1(&input);
+    println!("Part 1: {part_1_answer}");
+
+    let part_2_answer = part_2(&input);
+    println!("Part 2: {part_2_answer}");
+}
+
+fn part_1(input: &Grid<Pipe>) -> usize {
+    let mut max_steps = 0;
+    let coords_to_search = get_start_connections(input);
+    let mut searched_coords: HashMap<Coordinate, usize> = HashMap::new();
+    let mut coords_to_search: Vec<(Coordinate, Direction, usize)> =
+        coords_to_search.iter().map(|x| (x.0, x.1, 1)).collect();
+
+    println!("{:?}", coords_to_search);
+    // Pop the item
+    // Get the item
+    while !coords_to_search.is_empty() {
+        let (coord, trajectory, steps) = coords_to_search.pop().unwrap();
+
+        // If we haven't searched this coordinate before
+        if searched_coords.insert(coord, steps) {
+            println!("We haven't seen {:?} before", coord);
+            let current_pipe = input.get(coord);
+            let new_trajectory = current_pipe.get_next_trajectory(trajectory).unwrap();
+            println!(
+                "current pipe {:?}, new trajectory: {:?}",
+                current_pipe, new_trajectory
+            );
+            let next_coord = input.get_neighbour(coord, new_trajectory).unwrap();
+            let next_steps = steps + 1;
+            if steps > max_steps {
+                max_steps = steps;
+            }
+            coords_to_search.push((next_coord, new_trajectory, next_steps));
+        }
+    }
+
+    max_steps
+}
+
+fn part_2(input: &Grid<Pipe>) -> u64 {
+    todo!();
+}
+
+fn get_start_connections(input: &Grid<Pipe>) -> Vec<(Coordinate, Direction)> {
+    // Find start
+    let start_coord = input.find_item(&Pipe::Start).unwrap();
+
+    let all_neighbours = input.get_cardinal_neighbour_coords_with_direction(start_coord);
+
+    let mut valid_neighbours: Vec<(Coordinate, Direction)> = vec![];
+
+    for (neighbour_coord, trajectory) in all_neighbours {
+        let pipe = input.get(neighbour_coord);
+        if let Some(_) = pipe.get_next_trajectory(trajectory) {
+            valid_neighbours.push((neighbour_coord, trajectory));
+        }
+    }
+
+    println!("Neighbours: {:?}", valid_neighbours);
+    valid_neighbours
+}
+
+fn parse_input(input: &str) -> Grid<Pipe> {
+    let mut grid = Grid::new();
+    for line in input.lines() {
+        grid.push_row(line.chars().map(|x| Pipe::get_pipe(x)).collect())
+    }
+    grid
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy, Hash, Default)]
+enum Pipe {
+    Vertical,
+    Horizontal,
+    NE,
+    NW,
+    SW,
+    SE,
+    Start,
+    #[default]
+    Empty,
+}
+
+impl Pipe {
+    fn get_pipe(input: char) -> Pipe {
+        match input {
+            '|' => Pipe::Vertical,
+            '-' => Pipe::Horizontal,
+            'L' => Pipe::NE,
+            'J' => Pipe::NW,
+            '7' => Pipe::SW,
+            'F' => Pipe::SE,
+            'S' => Pipe::Start,
+            _ => Pipe::Empty,
+        }
+    }
+
+    fn get_next_trajectory(
+        &self,
+        // Direction we're moving in
+        // So if East, we're on the west side of the pipe
+        trajectory: Direction,
+        // Returns the trajectory if valid
+    ) -> Option<Direction> {
+        match trajectory {
+            Direction::North => match self {
+                Pipe::Vertical => Some(Direction::North),
+                Pipe::SE => Some(Direction::East),
+                Pipe::SW => Some(Direction::West),
+                _ => None,
+            },
+            Direction::South => match self {
+                Pipe::Vertical => Some(Direction::South),
+                Pipe::NE => Some(Direction::East),
+                Pipe::NW => Some(Direction::West),
+                _ => None,
+            },
+            Direction::East => match self {
+                Pipe::Horizontal => Some(Direction::East),
+                Pipe::NW => Some(Direction::North),
+                Pipe::SW => Some(Direction::South),
+                _ => None,
+            },
+            Direction::West => match self {
+                Pipe::Horizontal => Some(Direction::West),
+                Pipe::NE => Some(Direction::North),
+                Pipe::SE => Some(Direction::South),
+                _ => None,
+            },
+            _ => {
+                panic!("Got a non-cardinal direction!");
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TESTINPUT: &str = include_str!("testinput.txt");
+
+    #[test]
+    fn part_1_test() {
+        let input = parse_input(TESTINPUT);
+        assert_eq!(part_1(&input), 7);
+    }
+
+    #[test]
+    fn part_2_test() {
+        let input = parse_input(TESTINPUT);
+        assert_eq!(part_2(&input), 5);
+    }
+}
---
+++ aoc_lib/src/lib.rs
@@ -0,0 +1,218 @@
+pub struct Grid<T> {
+    data: Vec<T>,
+    row_len: usize,
+    row_count: usize,
+}
+
+impl<T> Grid<T> {
+    pub fn new() -> Self
+    where
+        T: Default,
+    {
+        Self {
+            data: vec![],
+            row_len: 0,
+            row_count: 0,
+        }
+    }
+
+    pub fn get(&self, coord: Coordinate) -> &T {
+        &self.data[coord.y * self.row_count + coord.x]
+    }
+
+    pub fn get_all_neighbour_coords(&self, location: Coordinate) -> Vec<Coordinate> {
+        let mut neighbours = self.get_cardinal_neighbour_coords(location);
+
+        let intercardinals = vec![
+            Direction::NorthEast,
+            Direction::NorthWest,
+            Direction::SouthEast,
+            Direction::SouthWest,
+        ];
+
+        for direction in intercardinals {
+            if let Some(neighbour) = self.get_neighbour(location, direction) {
+                neighbours.push(neighbour)
+            }
+        }
+
+        neighbours
+    }
+
+    pub fn get_cardinal_neighbour_coords(&self, location: Coordinate) -> Vec<Coordinate> {
+        let mut neighbours = vec![];
+        let cardinals = vec![
+            Direction::North,
+            Direction::South,
+            Direction::East,
+            Direction::West,
+        ];
+
+        for direction in cardinals {
+            if let Some(neighbour) = self.get_neighbour(location, direction) {
+                neighbours.push(neighbour);
+            }
+        }
+
+        neighbours
+    }
+
+    pub fn get_all_neighbour_coords_with_direction(
+        &self,
+        location: Coordinate,
+    ) -> Vec<(Coordinate, Direction)> {
+        let mut neighbours = self.get_cardinal_neighbour_coords_with_direction(location);
+
+        let intercardinals = vec![
+            Direction::NorthEast,
+            Direction::NorthWest,
+            Direction::SouthEast,
+            Direction::SouthWest,
+        ];
+
+        for direction in intercardinals {
+            if let Some(neighbour) = self.get_neighbour(location, direction) {
+                neighbours.push((neighbour, direction));
+            }
+        }
+
+        neighbours
+    }
+
+    pub fn get_cardinal_neighbour_coords_with_direction(
+        &self,
+        location: Coordinate,
+    ) -> Vec<(Coordinate, Direction)> {
+        let mut neighbours = vec![];
+        let cardinals = vec![
+            Direction::North,
+            Direction::South,
+            Direction::East,
+            Direction::West,
+        ];
+
+        for direction in cardinals {
+            if let Some(neighbour) = self.get_neighbour(location, direction) {
+                neighbours.push((neighbour, direction));
+            }
+        }
+
+        neighbours
+    }
+
+    pub fn push_row(&mut self, new_row: Vec<T>) {
+        if self.data.is_empty() {
+            self.row_len = new_row.len();
+        }
+
+        assert!(self.row_len == new_row.len());
+        self.row_count += 1;
+        self.data.extend(new_row);
+    }
+
+    pub fn get_neighbour(&self, location: Coordinate, direction: Direction) -> Option<Coordinate> {
+        let new_x = match direction {
+            Direction::East | Direction::NorthEast | Direction::SouthEast => {
+                if location.x + 1 < self.row_len {
+                    location.x + 1
+                } else {
+                    return None;
+                }
+            }
+            Direction::West | Direction::NorthWest | Direction::SouthWest => {
+                if location.x != 0 {
+                    location.x - 1
+                } else {
+                    return None;
+                }
+            }
+            _ => location.x,
+        };
+
+        let new_y = match direction {
+            Direction::North | Direction::NorthEast | Direction::NorthWest => {
+                if location.y != 0 {
+                    location.y - 1
+                } else {
+                    return None;
+                }
+            }
+            Direction::South | Direction::SouthEast | Direction::SouthWest => {
+                if location.x + 1 < self.row_len {
+                    location.y + 1
+                } else {
+                    return None;
+                }
+            }
+            _ => location.y,
+        };
+
+        Some(Coordinate { x: new_x, y: new_y })
+    }
+
+    pub fn is_valid_coord(&self, coord: Coordinate) -> bool {
+        coord.x < self.row_len && coord.y < self.data.len()
+    }
+
+    pub fn indexed_iter(&self) -> impl Iterator<Item = (Coordinate, &T)> {
+        self.data.iter().enumerate().map(move |(idx, i)| {
+            let position = Coordinate {
+                x: idx % self.row_len,
+                y: idx / self.row_len,
+            };
+            (position, i)
+        })
+    }
+
+    pub fn find_item(&self, input: &T) -> Option<Coordinate>
+    where
+        T: Eq,
+    {
+        for (coord, value) in self.indexed_iter() {
+            if value == input {
+                return Some(coord);
+            }
+        }
+
+        return None;
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy, Hash)]
+pub enum Direction {
+    North,
+    South,
+    East,
+    West,
+    NorthEast,
+    NorthWest,
+    SouthEast,
+    SouthWest,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy, Hash)]
+pub struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+impl Coordinate {
+    pub fn get_distance(&self, other: Coordinate) -> usize {
+        return &self.x.abs_diff(other.x) + &self.y.abs_diff(other.y);
+    }
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
