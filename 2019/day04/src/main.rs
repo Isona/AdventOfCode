069dfd6e88ab87da73326aa27c37a44419ae94d8
Izzy Whistlecroft <isona7@hotmail.co.uk>
2025-01-02 14:48:17 +0000

    Optimise 2019 day 2

--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -4,31 +4,31 @@ const INPUT: (i32, i32) = (137683, 596253);
 
 fn main() {
     let start = std::time::Instant::now();
-    let part_1_answer = part_1(&INPUT);
-
+    let part_1_matches = part_1(&INPUT);
+    let part_1_answer = part_1_matches.len();
     let time_taken = start.elapsed().as_secs_f32() * 1000.0;
     println!("Part 1: {part_1_answer} in {time_taken:.3} ms");
 
     let start = std::time::Instant::now();
-    let part_2_answer = part_2(&INPUT);
+    let part_2_answer = part_2(part_1_matches);
 
     let time_taken = start.elapsed().as_secs_f32() * 1000.0;
     println!("Part 2: {part_2_answer} in {time_taken:.3} ms");
 }
 
-fn part_1(range: &(i32, i32)) -> usize {
-    (range.0..range.1).filter(meets_criteria_part_1).count()
+fn part_1(range: &(i32, i32)) -> Vec<i32> {
+    (range.0..range.1).filter(meets_criteria_part_1).collect()
 }
 
 fn meets_criteria_part_1(input_number: &i32) -> bool {
     let mut adjacent = false;
-    for (digit_1, digit_2) in input_number.to_string().chars().tuple_windows() {
+    for (digit_1, digit_2) in digits(*input_number).tuple_windows() {
         // Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-        if digit_2.to_digit(10).unwrap() < digit_1.to_digit(10).unwrap() {
+        if digit_2 < digit_1 {
             return false;
         }
         // Two adjacent digits are the same
-        if digit_1 == digit_2 {
+        if !adjacent && digit_1 == digit_2 {
             adjacent = true;
         }
     }
@@ -36,23 +36,19 @@ fn meets_criteria_part_1(input_number: &i32) -> bool {
     adjacent
 }
 
-fn part_2(range: &(i32, i32)) -> usize {
-    (range.0..range.1).filter(meets_criteria_part_2).count()
+fn part_2(range: Vec<i32>) -> usize {
+    range
+        .iter()
+        .filter(|input_number: &&i32| meets_criteria_part_2(**input_number))
+        .count()
 }
 
-fn meets_criteria_part_2(input_number: &i32) -> bool {
-    for (digit_1, digit_2) in input_number.to_string().chars().tuple_windows() {
-        // Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-        if digit_2.to_digit(10).unwrap() < digit_1.to_digit(10).unwrap() {
-            return false;
-        }
-    }
-
+fn meets_criteria_part_2(input_number: i32) -> bool {
     let mut last_digit = 10;
     let mut last_digit_count = 0;
-    for digit in input_number.to_string().chars() {
+    for digit in digits(input_number) {
         // Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-        let real_digit = digit.to_digit(10).unwrap();
+        let real_digit = digit;
         if real_digit != last_digit {
             if last_digit_count == 2 {
                 return true;
@@ -67,6 +63,25 @@ fn meets_criteria_part_2(input_number: &i32) -> bool {
     last_digit_count == 2
 }
 
+// Stolen from https://stackoverflow.com/questions/41536479/how-do-i-split-an-integer-into-individual-digits
+fn digits(mut num: i32) -> impl Iterator<Item = i32> {
+    let mut divisor = 1;
+    while num >= divisor * 10 {
+        divisor *= 10;
+    }
+
+    std::iter::from_fn(move || {
+        if divisor == 0 {
+            None
+        } else {
+            let v = num / divisor;
+            num %= divisor;
+            divisor /= 10;
+            Some(v)
+        }
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,8 +94,8 @@ mod tests {
     }
     #[test]
     fn part_2_test() {
-        assert!(meets_criteria_part_2(&112233));
-        assert!(!meets_criteria_part_2(&123444));
-        assert!(meets_criteria_part_2(&111122));
+        assert!(meets_criteria_part_2(112233));
+        assert!(!meets_criteria_part_2(123444));
+        assert!(meets_criteria_part_2(111122));
     }
 }
