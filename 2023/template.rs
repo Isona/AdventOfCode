9706c11cd10ccec0bd95d5cd56db7aa314db4a97
Izzy Whistlecroft <isona7@hotmail.co.uk>
2025-06-22 17:42:46 +0100

    Update 2023 template.rs

--- 2023/template.rs
+++ 2023/template.rs
@@ -3,11 +3,17 @@ const INPUT: &str = include_str!("input.txt");
 fn main() {
     let input = parse_input(INPUT);
 
+    let start = std::time::Instant::now();
     let part_1_answer = part_1(&input);
-    println!("Part 1: {part_1_answer}");
 
+    let time_taken = start.elapsed().as_secs_f32() * 1000.0;
+    println!("Part 1: {part_1_answer} in {time_taken:.3} ms");
+
+    let start = std::time::Instant::now();
     let part_2_answer = part_2(&input);
-    println!("Part 2: {part_2_answer}");
+
+    let time_taken = start.elapsed().as_secs_f32() * 1000.0;
+    println!("Part 2: {part_2_answer} in {time_taken:.3} ms");
 }
 
 fn part_1(input: &[u64]) -> u64 {
