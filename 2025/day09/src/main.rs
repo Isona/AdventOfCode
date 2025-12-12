f59c4bf4b37136ec489923d49c9c70c02150729e
Izzy Whistlecroft <isona7@hotmail.co.uk>
2025-12-12 13:38:00 +0000

    Add 2025 day 09 part 1

---
+++ 2025/day09/src/main.rs
@@ -0,0 +1,63 @@
+use aoc_lib::Coordinate;
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    let input = parse_input(INPUT);
+
+    let start = std::time::Instant::now();
+    let part_1_answer = part_1(&input);
+
+    let time_taken = start.elapsed().as_secs_f32() * 1000.0;
+    println!("Part 1: {part_1_answer} in {time_taken:.3} ms");
+
+    let start = std::time::Instant::now();
+    let part_2_answer = part_2(&input);
+
+    let time_taken = start.elapsed().as_secs_f32() * 1000.0;
+    println!("Part 2: {part_2_answer} in {time_taken:.3} ms");
+}
+
+fn part_1(input: &[Coordinate]) -> usize {
+    let mut max_square_size = 0;
+    for (first, second) in input.iter().tuple_combinations() {
+        let square_size = (first.x.abs_diff(second.x) + 1) * (first.y.abs_diff(second.y) + 1);
+        max_square_size = max_square_size.max(square_size);
+    }
+
+    max_square_size
+}
+
+fn part_2(input: &[Coordinate]) -> u64 {
+    todo!();
+}
+
+fn parse_input(input: &str) -> Vec<Coordinate> {
+    input.lines().map(get_coord).collect()
+}
+
+fn get_coord(input: &str) -> Coordinate {
+    let mut split = input.split(',');
+    let x = split.next().unwrap().parse().unwrap();
+    let y = split.next().unwrap().parse().unwrap();
+    Coordinate { x, y }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TESTINPUT: &str = include_str!("testinput.txt");
+
+    #[test]
+    fn part_1_test() {
+        let input = parse_input(TESTINPUT);
+        assert_eq!(part_1(&input), 50);
+    }
+
+    #[test]
+    fn part_2_test() {
+        let input = parse_input(TESTINPUT);
+        assert_eq!(part_2(&input), 24);
+    }
+}
